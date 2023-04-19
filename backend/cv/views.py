5abff207d8a7adeae00eb08505127735b7409eb4
Max Verbitskiy <maxxtor@yandex.ru>
2023-04-19 10:30:55 +0400

    fix templates dirs

--- backend/cv/views.py
+++ backend/cv/views.py
@@ -2,8 +2,8 @@ from django.views.generic.base import TemplateView
 
 
 class MaxxtorCvView(TemplateView):
-    template_name = 'maxxtor.html'
+    template_name = 'cv/maxxtor.html'
 
 
 class AlexshowCvView(TemplateView):
-    template_name = 'alexshow.html'
+    template_name = 'cv/alexshow.html'
